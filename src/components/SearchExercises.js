26bccacd450dc86587224eddab5a07065d9d7a39
pcbennett108 <pcbennett42@gmail.com>
2022-10-13 12:36:59 -0600

    Add BodyPart component and connect it to Search and Home

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,7 +4,7 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
   const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
@@ -80,7 +80,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
