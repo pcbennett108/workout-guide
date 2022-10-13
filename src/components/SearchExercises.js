b32f7cb7fd1b7b88e5321f1bce42195e5ca1014c
pcbennett108 <pcbennett42@gmail.com>
2022-10-13 11:10:15 -0600

    add .env to gitignore

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -5,7 +5,18 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
-  const { exercises, setExercises } = useState([]);
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+  }, []);
 
   const handleSearch = async () => {
     if (search) {
